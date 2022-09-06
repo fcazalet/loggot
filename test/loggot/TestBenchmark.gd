0fe440fb8aba48635d114a133ef03ca3d123e103
fcazalet <florian.cazalet@free.fr>
2022-09-06 19:42:54 +0200

    Removed extends Object to use instances auto free()

--- test/loggot/TestBenchmark.gd
+++ test/loggot/TestBenchmark.gd
@@ -1,11 +1,15 @@
 extends Control
 
+
 var logger_test : LoggotLogger
+var log_counter = 0
 
 func _ready():
 	logger_test = Loggot.get_logger("logger_test")
 	logger_test.attach_appender(LoggotAsyncAppender.new(LoggotConsoleAppender.new()))
+	logger_test.attach_appender(LoggotAsyncAppender.new(LoggotConsoleAppender.new()))
 
 
 func _process(delta):
-	logger_test.info("Test Log. Delta: {}", [delta])
+	log_counter += 2
+	logger_test.info("Log messages: {}", [log_counter])
